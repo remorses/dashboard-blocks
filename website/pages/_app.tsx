40619517596f91e5d1bf7f5a5f054f47024e9b6d
remorses <beats.by.morse@gmail.com>
2020-06-13 18:43:10 +0200

    added main site

---
+++ website/pages/_app.tsx
@@ -0,0 +1,33 @@
+import { DokzProvider, GithubLink, ColorModeSwitch } from 'dokz/dist'
+import React, { Fragment } from 'react'
+import Head from 'next/head'
+
+export default function App(props) {
+    const { Component, pageProps } = props
+    return (
+        <Fragment>
+            <Head>
+                <link
+                    href='https://fonts.googleapis.com/css?family=Fira+Code'
+                    rel='stylesheet'
+                    key='google-font-Fira'
+                />
+            </Head>
+            <DokzProvider
+                docsRootPath='pages/docs'
+                headerItems={[
+                    <GithubLink
+                        key='0'
+                        url='https://github.com/remorses/dokz'
+                    />,
+                    <ColorModeSwitch key='1' />,
+                ]}
+                sidebarOrdering={{
+                    'index.mdx': true,
+                }}
+            >
+                <Component {...pageProps} />
+            </DokzProvider>
+        </Fragment>
+    )
+}
---
+++ website/pages/index.tsx
@@ -0,0 +1,5 @@
+import Stack from '@chakra-ui/core/dist/Stack'
+
+export default function Page({}) {
+    return <Stack></Stack>
+}
