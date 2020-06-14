646b393444cc368a604b903a6194001504316149
remorses <beats.by.morse@gmail.com>
2020-06-14 18:21:42 +0200

    made table component

--- dashboard-blocks/src/Table.tsx
+++ dashboard-blocks/src/Table.tsx
@@ -14,10 +14,60 @@ import {
     Link,
     Stack,
     Flex,
+    StackProps,
 } from '@chakra-ui/core'
 
-export const Table = ({}) => {
-    return <Stack>
-        
-    </Stack>
+export type TableProps = {
+    head: ReactNode[]
+    rows: ReactNode[][]
+} & StackProps
+
+export const Table = ({ head, rows, ...rest }) => {
+    return (
+        <Stack overflowX='auto' direction='row' {...rest}>
+            <Box as='table' textAlign='left' width='full'>
+                <Box as='tr'>
+                    {head.map((name) => {
+                        return <THead>{name}</THead>
+                    })}
+                </Box>
+                {rows.map((row) => {
+                    return (
+                        <Box as='tr'>
+                            {row.map((value) => {
+                                return <TData>{value}</TData>
+                            })}
+                        </Box>
+                    )
+                })}
+            </Box>
+        </Stack>
+    )
 }
+
+const THead = (props) => {
+    const { colorMode } = useColorMode()
+    const bg = { light: 'gray.50', dark: 'whiteAlpha.100' }
+    return (
+        <Box
+            as='th'
+            bg={bg[colorMode]}
+            fontWeight='semibold'
+            p={2}
+            fontSize='sm'
+            {...props}
+        />
+    )
+}
+
+const TData = (props) => (
+    <Box
+        as='td'
+        p={2}
+        borderTopWidth='1px'
+        borderColor='inherit'
+        fontSize='sm'
+        whiteSpace='normal'
+        {...props}
+    />
+)
