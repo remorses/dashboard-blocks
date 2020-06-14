ccd0a22a81089af61ed7691628bc6a9902bcefa9
remorses <beats.by.morse@gmail.com>
2020-06-14 18:39:14 +0200

    made table style

--- dashboard-blocks/src/Table.tsx
+++ dashboard-blocks/src/Table.tsx
@@ -26,7 +26,14 @@ export type TableProps = {
 
 export const TableBlock = ({ head = [], heading = '', rows = [], ...rest }) => {
     return (
-        <Block heading={heading} headingHeight='0px' p='0px' overflowX='auto' direction='row' {...rest}>
+        <Block
+            heading={heading}
+            headingHeight='0px'
+            p='0px'
+            overflowX='auto'
+            direction='row'
+            {...rest}
+        >
             <Box as='table' textAlign='left' width='full'>
                 <Box as='tr'>
                     {head.map((name, i) => {
@@ -47,16 +54,18 @@ export const TableBlock = ({ head = [], heading = '', rows = [], ...rest }) => {
     )
 }
 
+const cellPadding= 6
+
 const THead = (props) => {
     const { colorMode } = useColorMode()
     const bg = { light: 'gray.50', dark: 'whiteAlpha.100' }
     return (
         <Box
             as='th'
-            bg={bg[colorMode]}
+            // bg={bg[colorMode]}
             fontWeight='semibold'
-            p={2}
-            fontSize='sm'
+            p={cellPadding}
+            // fontSize='1.2em'
             {...props}
         />
     )
@@ -65,10 +74,10 @@ const THead = (props) => {
 const TData = (props) => (
     <Box
         as='td'
-        p={2}
+        p={cellPadding}
         borderTopWidth='1px'
         borderColor='inherit'
-        fontSize='sm'
+        // fontSize='sm'
         whiteSpace='normal'
         {...props}
     />
